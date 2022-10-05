702523e5ad83eb178a8a7e8760ab90ada7e91211
yanakapura <y.kapura@gmail.com>
2022-10-05 17:01:55 +0300

    Добавлены модальные окна
    Добавлены модальные окна регистрации и входа

--- js/script.js
+++ js/script.js
@@ -1,13 +1,54 @@
-document.querySelector('.hero-section__button').addEventListener("mouseout", (e)=> {
-    outAnimation()
-})
+const signupBtn = document.querySelector(".nav__sign-up");
+const loginBtn = document.querySelector(".nav__login");
+
+const heroBtn = document.querySelector(".hero-section__button");
+const heroCircle = document.querySelector(".circle");
+
+const overflow = document.querySelector(".overflow");
+const modalSignUp = document.querySelector("#modal--sign-up");
+const modalLogin = document.querySelector("#modal--login");
+
+heroBtn.addEventListener("mouseout", (e) => {
+  outAnimation();
+});
 
 function outAnimation() {
-    document.querySelector('.circle').classList.add("out")
-    document.querySelector('.hero-section__button').classList.add("out")
-    
-    setTimeout(() => {
-        document.querySelector('.circle').classList.remove("out")
-    document.querySelector('.hero-section__button').classList.remove("out")
-    }, 300);
+  heroCircle.classList.add("out");
+  heroBtn.classList.add("out");
+
+  setTimeout(() => {
+    heroCircle.classList.remove("out");
+    heroBtn.classList.remove("out");
+  }, 300);
+}
+
+signupBtn.addEventListener("click", () => {
+  openRegistration();
+});
+loginBtn.addEventListener("click", () => {
+  openLogin();
+});
+
+overflow.addEventListener("click", (e) => {
+  if (e.target.classList.contains("overflow") || e.target.classList.contains("modal__close")) {
+    closeLogin();
+    closeRegistration();
+  }
+});
+
+function openRegistration() {
+  overflow.style.display = "block";
+  modalSignUp.style.display = "block";
+}
+function closeRegistration() {
+  overflow.style.display = "none";
+  modalSignUp.style.display = "none";
+}
+function openLogin() {
+  overflow.style.display = "block";
+  modalLogin.style.display = "block";
+}
+function closeLogin() {
+  overflow.style.display = "none";
+  modalLogin.style.display = "none";
 }
