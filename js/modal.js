919d8fc175ae64fea825d8b2ea6584d55f177d20
Valeria <tripuzvaleria@gmail.com>
2022-10-08 21:39:26 +0300

    validation

--- js/modal.js
+++ js/modal.js
@@ -7,33 +7,35 @@ const modalLogin = document.querySelector("#modal--login");
 
 // Открыть/закрыть модальные окна
 signupBtn.addEventListener("click", () => {
-    openRegistration();
-  });
-  loginBtn.addEventListener("click", () => {
-    openLogin();
-  });
-  
-  overflow.addEventListener("click", (e) => {
-    if (e.target.classList.contains("overflow") || e.target.classList.contains("modal__close")) {
-      closeLogin();
-      closeRegistration();
-    }
-  });
-  
-  function openRegistration() {
-    overflow.style.display = "block";
-    modalSignUp.style.display = "block";
+  openRegistration();
+});
+loginBtn.addEventListener("click", () => {
+  openLogin();
+});
+
+overflow.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("overflow") ||
+    e.target.classList.contains("modal__close")
+  ) {
+    closeLogin();
+    closeRegistration();
   }
-  function closeRegistration() {
-    overflow.style.display = "none";
-    modalSignUp.style.display = "none";
-  }
-  function openLogin() {
-    overflow.style.display = "block";
-    modalLogin.style.display = "block";
-  }
-  function closeLogin() {
-    overflow.style.display = "none";
-    modalLogin.style.display = "none";
-  }
-  
+});
+
+function openRegistration() {
+  overflow.style.display = "block";
+  modalSignUp.style.display = "block";
+}
+function closeRegistration() {
+  overflow.style.display = "none";
+  modalSignUp.style.display = "none";
+}
+function openLogin() {
+  overflow.style.display = "block";
+  modalLogin.style.display = "block";
+}
+function closeLogin() {
+  overflow.style.display = "none";
+  modalLogin.style.display = "none";
+}
